6c5791e227132a976beba20fbde9276cf97688ca
Daniel Faculdade <you@example.com>
2025-07-21 21:58:04 -0300

    Inicio do novo codigo de calculo do cavalo, projeto incrementado com Variante, possibilidade de escolha de armadura e sela

---
+++ CODIGOS/cavalo_versao_2.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#include <Windows.h>
+
+/*Bibliotecas pra C++*/
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+float calculo (float z) {
+
+	const x = ;
+	const y = ;
+	float res;
+
+	res = (z*y)/x
+
+	      return res;
+
+}
+
+
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	int Health, Color;
+	float Jump, Speed, Fjump Fspeed;
+
+
+
+
+
+
+
+
+//	print the command on the console so the user can copy it
+cout << "/minecraft:summon horse ~ ~1 ~ {Health:" << Health <<"f,Temper:100,Variant:" << Color << "0,CustomName:'\"" << Name << "\"',ArmorItems:[{},{},{id:\"minecraft:diamond_horse_armor\",count:1},{}],attributes:[{id:\"minecraft:jump_strength\",base:" << Fjump << ",{id:\"minecraft:max_health\",base:" << Health <<"},{id:\"minecraft:movement_speed\",base:{Speed}}],SaddleItem:{id:\"minecraft:saddle\",count:1b}}"; endl;
+
+	return 0;
+}
