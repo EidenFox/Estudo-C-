e7de17b1a2aca0cbabb57e613f6111ca3c1dbf3c
Daniel Faculdade <you@example.com>
2025-09-25 20:35:42 -0300

    Terminar trabalho

--- CODIGOS/Trabalho/Menu.cpp
+++ CODIGOS/Trabalho/Menu.cpp
@@ -10,7 +10,16 @@
 #include <string>
 using namespace std;
 
-
+void listarDisponiveis();
+void cadastroCliente();
+void solicitarReparo();
+void listarAlugadas();
+void devolucao();
+void alugar();
+	
+bool checarLogin(){
+	return true;
+}
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -43,6 +52,7 @@ int main() {
 	
 	string email, senha;
 	bool continuar = false;
+	int op;
 	
 	do{
 		cout << "--------------------\n--------MENU--------\n--------------------\n" << endl;
@@ -50,13 +60,59 @@ int main() {
 		cout << "Digite seu Email: "; cin >> email;
 		cout << "Digite sua Senha: "; cin >> senha;
 		
-		if (checarLogin(email, senha){
+		if (checarLogin()){
 			continuar = true;
 		}else{
 			system("cls");
 			cout << "Dados incorretos, tente novamente!" << endl;
 		}
 	}while(!continuar);
+	
+	do{
+		cout << "[1] Cadastrar Funcionário" << endl;
+		cout << "[0] Ir para o Menu." << endl;
+		cin >> op;
+	}while (op!=0);
+	op = 1;
+	
+	do{
+		cout << "--------------------\n--------MENU--------\n--------------------\n" << endl;
+		cout << "[1] Cadastrar Cliente" << endl;
+		cout << "[2] Listar Bicicletas" << endl;
+		cout << "[3] Alugar Bicicleta" << endl;
+		cout << "[4] Checar Bicibletas alugadas" << endl;
+		cout << "[5] Solicitar Reparo" << endl;
+		cout << "[6] Registrar Devolução" << endl;
+		cout << "[0] Sair" << endl;
+		cin >> op;
+		
+		switch(op){
+			case 1:
+//				cadastroCliente();
+				break;
+			case 2:
+//				listarDisponiveis();
+				break;
+			case 3:
+//				alugar();
+				break;
+			case 4:
+//				listarAlugadas();
+				break;
+			case 5:
+//				solicitarReparo();
+			case 6:
+//				devolucao();
+				break;
+			case 0:
+				cout << "Saindo";
+				for (int i = 0; i > 5; i++){
+					sleep(500);
+					cout << "." << endl;
+				}
+		}
+		
+	}while (op != 0);
 
 
 
